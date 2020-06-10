657d85f825c37f0156dfa6c047a2847df76132dd
ecr23xx <370095872@qq.com>
2020-06-10 09:27:14 +0800

    backward support

--- benchmark.py
+++ benchmark.py
@@ -1,11 +1,11 @@
 import torch
-from torchprof import Profile
+import torch.nn as nn
 from misc import log_model_info
+from profile import Profile, BackwardProfile
 from collections import namedtuple, defaultdict, OrderedDict
 
 
 def walk_modules(module, name="", path=(), pattern=[]):
-    """Generator. Walks through a PyTorch Module and outputs Trace tuples"""
     if not name:
         name = module.__class__.__name__
     named_children = list(module.named_children())
@@ -20,19 +20,38 @@ def walk_modules(module, name="", path=(), pattern=[]):
 def benchmark_run(model, is_3d=False):
     log_model_info(model, is_3d)
     device = torch.cuda.current_device()
-    if is_3d:
-        inputs = [torch.rand(8, 3, 16, 224, 224).to(device)]
-    else:
-        inputs = torch.rand(8, 3, 224, 224).to(device)
 
-    if is_3d:
-        pattern = ["b"]
-        paths = list(walk_modules(model, pattern=pattern))
-        paths = None
-    else:
-        pattern = []
-        paths = None
+    pattern = ["b"]
+    paths = list(walk_modules(model, pattern=pattern))
+    paths = None
+
+    labels = torch.empty(8, dtype=torch.long).random_(400).to(device)
+    loss_func = nn.CrossEntropyLoss(reduction='mean').to(device)
 
     with Profile(model, paths=paths, use_cuda=True) as prof:
-        model(inputs)
+        inputs = [torch.rand(8, 3, 16, 224, 224).to(device)]
+        output = model(inputs)
+        loss = loss_func(output, labels)
+        loss.backward()
+    print("Forward pass")
     print(prof.display(show_events=False))
+    print("")
+
+    print("Backward pass")
+    with BackwardProfile(model, paths=paths, use_cuda=True) as bprof:
+        inputs = [torch.rand(8, 3, 16, 224, 224).to(device)]
+        output = model(inputs)
+        loss = loss_func(output, labels)
+        loss.backward()
+    print(bprof.display(show_events=False))
+    print("")
+
+    with torch.autograd.profiler.profile(use_cuda=True) as prof:
+        for _ in range(10):
+            inputs = [torch.rand(8, 3, 16, 224, 224).to(device)]
+            output = model(inputs)
+            loss = loss_func(output, labels)
+            loss.backward()
+
+    print(prof.key_averages().table(sort_by="cpu_time_total"))
+    print(prof.key_averages().table(sort_by="cuda_time_total"))
